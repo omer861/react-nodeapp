bf73b57611fa9f75c5733fa8c6cf94e06a765b36
syed omer <omersyed861@gmail.com>
2025-08-04 16:25:48 +0530

    update code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,17 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
-    strictPort: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  },
-  optimizeDeps: {
-    exclude: ['crypto']
-  },
-  define: {
-    global: 'globalThis'
+    port: 5173
   }
 })
